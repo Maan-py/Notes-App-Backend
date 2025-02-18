a12d4290b2f6a902afb7a09fb12f7292857b398f
Maan-py <mluqmaan22@gmail.com>
2025-02-18 20:18:44 +0700

    authentication implemented

--- src/server.js
+++ src/server.js
@@ -1,24 +1,33 @@
+// mengimpor dotenv dan menjalankan konfigurasinya
 require("dotenv").config();
+
 const Hapi = require("@hapi/hapi");
-const ClientError = require("./exceptions/ClientError");
+const Jwt = require("@hapi/jwt");
 
 // notes
 const notes = require("./api/notes");
-// const NotesService = require("./services/inMemory/NotesService");
 const NotesService = require("./services/postgres/NotesService");
 const NotesValidator = require("./validator/notes");
+const ClientError = require("./exceptions/ClientError");
 
-// user
+// users
 const users = require("./api/users");
 const UsersService = require("./services/postgres/UsersService");
 const UsersValidator = require("./validator/users");
 
+// authentications
+const authentications = require("./api/authentications");
+const AuthenticationsService = require("./services/postgres/AuthenticationsService");
+const TokenManager = require("./tokenize/TokenManager");
+const AuthenticationsValidator = require("./validator/authentications");
+
 const init = async () => {
   const notesService = new NotesService();
   const usersService = new UsersService();
+  const authenticationsService = new AuthenticationsService();
+
   const server = Hapi.server({
     port: process.env.PORT,
-    // host: process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0"
     host: process.env.HOST,
     routes: {
       cors: {
@@ -26,7 +35,33 @@ const init = async () => {
       },
     },
   });
-  // server.route(routes);
+
+  // registrasi plugin eksternal
+  await server.register([]);
+
+  // registrasi plugin eksternal
+  await server.register([
+    {
+      plugin: Jwt,
+    },
+  ]);
+
+  // mendefinisikan strategy autentikasi jwt
+  server.auth.strategy("notesapp_jwt", "jwt", {
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id,
+      },
+    }),
+  });
 
   await server.register([
     {
@@ -41,6 +76,15 @@ const init = async () => {
       options: {
         service: usersService,
         validator: UsersValidator,
+      },
+    },
+    {
+      plugin: authentications,
+      options: {
+        authenticationsService,
+        usersService,
+        tokenManager: TokenManager,
+        validator: AuthenticationsValidator,
       },
     },
   ]);
